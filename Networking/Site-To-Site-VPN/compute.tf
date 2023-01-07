872160e652d432cdc9cf8b7d68e8733fe6e922d8
Luis Rodríguez <luis.sanmartin.rodriguez@gmail.com>
2023-01-07 22:38:46 +0100

    Update

---
+++ Networking/Site-To-Site-VPN/compute.tf
@@ -0,0 +1,35 @@
+resource "azurerm_network_interface" "vm" {
+  name                = "pip-vm-${local.suffix}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.utility.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "utility-machine"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B2s"
+  admin_username      = "admin"
+  admin_password      = "yourpassword"
+  network_interface_ids = [
+    azurerm_network_interface.vm.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
